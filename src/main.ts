1fb0ddb35d074e5de4a0b0ec5ed52759d01b4020
uxuip <uxuip@outlook.com>
2024-06-18 13:41:27 +0800

    fix: 修复升级依赖导致的问题

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,17 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router/auto'
 import { setupLayouts } from 'virtual:generated-layouts'
+import { routes } from 'vue-router/auto-routes'
 import App from './App.vue'
 
 import '@unocss/reset/tailwind.css'
+import 'virtual:uno.css'
 import './styles/main.css'
-import 'uno.css'
 
 const app = createApp(App)
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  extendRoutes: routes => setupLayouts(routes),
+  routes: setupLayouts(routes),
 })
 app.use(router)
 app.mount('#app')
